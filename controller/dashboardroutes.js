268a66527276bf183a31d4206e69771f822d4efd
Kenny Boyer <ksboyerii@gmail.com>
2021-05-05 16:39:53 -0500

    working on dashboard routes

--- controller/dashboardroutes.js
+++ controller/dashboardroutes.js
@@ -1,5 +1,26 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const {Post, User, Comment } = require("../models");
+const withAuth = require('../utils/auth');
 
-router.get('/',)
+router.get('/', withAuth,(req, res)=> {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'title', 
+            'created_date',
+            'post_text'
+        ],
+        include: [{
+                model: Comment,
+                arritbutes: ['id', 'post_test','created_date','user_id'],
+                include: {
+                    model:User,
+                    attributes: ['username']
+                }
+        }] 
+    })
+})
